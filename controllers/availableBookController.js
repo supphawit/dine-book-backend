8617a14839babb874e046fbd2b758cbaa9ff5123
supphawit <s.wuttiprasit@gmail.com>
2025-04-06 06:42:26 +0700

    Update environment configuration, add mock data, and implement seed script for database migration

--- controllers/availableBookController.js
+++ controllers/availableBookController.js
@@ -1,5 +1,111 @@
 const AvailableBook = require("../models/availableBookModel");
 
+const generateAvailableBooksForDay = (_date) => {
+  const times = [
+    "11:00",
+    "11:30",
+    "12:00",
+    "12:30",
+    "13:00",
+    "13:30",
+    "14:00",
+    "17:00",
+    "17:30",
+    "18:00",
+    "18:30",
+    "19:00",
+    "19:30",
+    "20:00",
+    "20:30",
+    "21:00",
+  ];
+
+  const _tables = [
+    {
+      _id: uuidv4(),
+      name: "โต๊ะริมหน้าต่าง 1",
+      capacity: 3,
+      location: "indoor",
+      isAvailable: true,
+      image: imageToBase64(
+        path.join(__dirname, "images/tables/table-window.jpeg")
+      ),
+    },
+    {
+      _id: uuidv4(),
+      name: "โต๊ะริมหน้าต่าง 2",
+      capacity: 4,
+      location: "indoor",
+      isAvailable: true,
+      image: imageToBase64(
+        path.join(__dirname, "images/tables/table-window.jpeg")
+      ),
+    },
+    {
+      _id: uuidv4(),
+      name: "โต๊ะกลาง 1",
+      capacity: 4,
+      location: "indoor",
+      isAvailable: true,
+      image: imageToBase64(
+        path.join(__dirname, "images/tables/table-center.jpg")
+      ),
+    },
+    {
+      _id: uuidv4(),
+      name: "โต๊ะกลาง 2",
+      capacity: 4,
+      location: "indoor",
+      isAvailable: true,
+      image: imageToBase64(
+        path.join(__dirname, "images/tables/table-center.jpg")
+      ),
+    },
+    {
+      _id: uuidv4(),
+      name: "โต๊ะมุม 1",
+      capacity: 6,
+      location: "indoor",
+      isAvailable: true,
+      image: imageToBase64(
+        path.join(__dirname, "images/tables/table-center.jpg")
+      ),
+    },
+    {
+      _id: uuidv4(),
+      name: "โต๊ะมุม 2",
+      capacity: 2,
+      location: "indoor",
+      isAvailable: true,
+      image: imageToBase64(
+        path.join(__dirname, "images/tables/table-center.jpg")
+      ),
+    },
+  ];
+
+  const timeSlots = [];
+  const targetDate = _date ? new Date(_date) : new Date();
+
+  const dateString = targetDate.toISOString().split("T")[0];
+
+  const daySlots = {
+    _id: uuidv4(),
+    date: dateString,
+    slots: times.map((time) => ({
+      _id: uuidv4(),
+      time,
+      isAvailable: Math.random() > 0.3,
+      tables: _tables.map((table) => ({
+        ...table,
+        _id: uuidv4(),
+        isAvailable: Math.random() > 0.3,
+      })),
+    })),
+  };
+
+  timeSlots.push(daySlots);
+  return timeSlots;
+};
 const getAvailableBooksByDate = async (req, res) => {
   try {
     const { date } = req.query;
@@ -20,13 +126,11 @@ const getAvailableBooksByDate = async (req, res) => {
       });
     }
 
-    const availableBook = await AvailableBook.findOne({ date });
+    let availableBook = await AvailableBook.findOne({ date });
 
     if (!availableBook) {
-      return res.status(404).json({
-        status: "error",
-        message: "No available bookings found for this date",
-      });
+      const newAvailableBook = generateAvailableBooksForDay();
+      availableBook = await AvailableBook.create(newAvailableBook);
     }
 
     res.json({
